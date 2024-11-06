dfe6163fa771e1f9440b9e13aefed1746305ab2d
Eduardo Prado <epradogr@gmail.com>
2024-11-06 15:10:37 -0600

    imagenes y links

--- app.py
+++ app.py
@@ -8,6 +8,42 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/git')
+def git():
+    return render_template('git.html')
+
+@app.route('/impresion3D')
+def impresion3D():
+    return render_template('impresion3D.html')
+
+@app.route('/sobremi')
+def sobremi():
+    return render_template('sobremi.html')
+
+@app.route('/arVrEr')
+def arVrEr():
+    return render_template('arVrEr.html')
+
+@app.route('/apps')
+def apps():
+    return render_template('apps.html')
+
+@app.route('/ahorcado')
+def ahorcado():
+    return render_template('ahorcado.html')
+
+@app.route('/desarrollo')
+def desarrollo():
+    return render_template('desarrollo.html')
+
+@app.route('/Tarea')
+def Tarea():
+    return render_template('Tarea.html')
+
+@app.route('/servidores')
+def servidores():
+    return render_template('servidores.html')
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
